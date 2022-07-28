41747546e5a0d3d10643546edf1516a51eb63e34
Yurham Afif <yurhamafif.edu@gmail.com>
2022-07-28 16:06:02 +0700

    [edit] add regex for email

--- src/component/login.js
+++ src/component/login.js
@@ -15,7 +15,8 @@ class Login extends Component {
     }
 
     handleUsernameChange = (event) => {
-        if (event.target.value.includes(`@`)) {
+        var validRegex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (event.target.value.match(validRegex)) {
             this.setState({
                 username : event.target.value,
                 usernameValid : true,
